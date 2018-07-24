3b8d9e250c610fa81c1904bfeb4af8bc20b3d151
Hania Bigo <hrbigos@umich.edu>
2018-07-24 02:51:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete newObj[key]
+  var newObj = delete newObj[key]
   return newObj
 }
